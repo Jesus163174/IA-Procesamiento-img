480f2a3ea236902aa378912284761cc19152810c
Adrian Oswaldo Morales Perez <adrian_oswalmorales@outlook.com>
2019-05-21 01:00:29 -0500

    modificacion

--- Test.py
+++ Test.py
@@ -2,13 +2,14 @@
 import numpy as np
 import cv2
 
-imagenPrincipal = np.empty(shape = (15,15), dtype = int)#declar una matriz
-mascara = np.empty(shape=(3,3), dtype= int )
+imagenPrincipal = np.empty(shape = (15,18), dtype = int)#declar una matriz
+mascara = np.empty(shape=(5,5), dtype= int )
 puntosSSD = np.empty(shape=())
 def main():
-    llenarMatrix()
+    # llenarMatrix()
     funcname()
     mostrar()
+    mascaraImagen()
     pass
 
 def funcname():
@@ -28,13 +29,14 @@ def llenarMatrix():
 
 def mostrar():
     '''  '''
-    columna = len(imagenPrincipal)
-    fila = len(imagenPrincipal[0]) 
+    fila = len(imagenPrincipal)
+    columna = len(imagenPrincipal[0]) 
     print( fila , columna)
     print (imagenPrincipal[7][7])
     pass
 
 def mascaraImagen():#pasar parametro del tamaño de la imagen, mascara
+    global mascara #hacer uso de la variable global
     ''' En esta practica se usara una mascara de 5x5, para que la posicion 2,2 sea el centro y se pueda recuperar 
         un punto SSD.
             - Concer cuantas veces entra la mascara en la imagen:
@@ -42,7 +44,22 @@ def mascaraImagen():#pasar parametro del tamaño de la imagen, mascara
                 filaImagenOriginal - filaMascara = numero de veces que entra la imagen la fila;
                 columanImagenOriginal - columnaMascara = numero de veces que entra la imagen en columna;
     '''
+    filaMascara = len(imagenPrincipal) - len(mascara)#Calcular el numero de veces que entra la mascara en las filas de la imagenPrincipal
+    columanMascara = len(imagenPrincipal[0])- len(mascara[0])#Calcular el numero de veces que entra la mascara en las columna de la imagenPrincipal
+    print(filaMascara,columanMascara)
+    
+    # for i in range(0,filaMascara):
+    #     for j in range(0,columanMascara):
+    #         calcularSSD()
+    #         pass
+    #     pass
 
+    pass
+
+def calcularSSD():
+
+    for i in range(0,len(mascara)):
+        pass
 
     pass
 
